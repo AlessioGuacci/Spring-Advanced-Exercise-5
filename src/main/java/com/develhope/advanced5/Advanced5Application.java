4318cacb1c072f77a266c0c497c45f38e6823aac
Alessio Guacci <alessioguacci@gmail.com>
2024-03-20 18:09:55 +0100

    Deploy 1

---
+++ src/main/java/com/develhope/advanced5/Advanced5Application.java
@@ -0,0 +1,13 @@
+package com.develhope.advanced5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Advanced5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Advanced5Application.class, args);
+	}
+
+}
